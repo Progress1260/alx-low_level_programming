2590749ec5d8378f823bffd50eb7b56a16ecf248
Progress1260 <truetoyourself45@gmail.com>
2023-02-16 14:07:41 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
@@ -7,3 +6,5 @@
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
